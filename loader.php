4def327032f65623a1ad72f654d6d383546dd3d8
lenasterg <lenasterg@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-10-17 07:34:32 +0000

    credits
    git-svn-id: https://plugins.svn.wordpress.org/ls-buddypress-activity-plus-tabs-extension/trunk@1008975 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- loader.php
+++ loader.php
@@ -8,7 +8,7 @@
   Requires at least: WP 3.5.1, BuddyPress 1.7
   Tested up to: WP 3.6.1, BuddyPress 1.8.1
   License:  GNU General Public License 3.0 or newer (GPL) http://www.gnu.org/licenses/gpl.html
-  Author: Lena Stergatu
+  Author: Lena Stergatu, NTS on cti.gr
   Author URI: http://lenasterg.wordpress.com)
 
 
